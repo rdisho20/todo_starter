86e3026bcd5160d8a0192041479c7e9367593b5b
rdisho20 <xnod_bs@yahoo.com>
2025-08-01 20:00:03 -0600

    adding more todo files

---
+++ todos/utils.py
@@ -0,0 +1,10 @@
+def error_for_list_title(title, lists):
+    if any(lst['title'] == title for lst in lists):
+        return "The title must be unique."
+    elif not 1 <= len(title) <= 100:
+        return "The title must be between 1 and 100 characters."
+    else:
+        return None
+
+def find_list_by_id(list_id, lists):
+    return next((lst for lst in lists if lst['id'] == list_id), None)
