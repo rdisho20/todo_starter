98d3f38655eecc0aa96dd5eb83e6de4c9198cdfb
rdisho20 <xnod_bs@yahoo.com>
2025-08-03 12:20:42 -0600

    updating todo_starter files

--- app.py
+++ app.py
@@ -11,7 +11,14 @@ from flask import (
 )
 
 from werkzeug.exceptions import NotFound
-from todos.utils import error_for_list_title, find_list_by_id
+
+from todos.utils import (
+    error_for_list_title,
+    error_for_todo,
+    find_list_by_id,
+    find_todo_by_id,
+    mark_all_completed
+)
 
 app = Flask(__name__)
 app.secret_key='secret1'
@@ -60,7 +67,76 @@ def show_list(list_id):
     if not lst:
         raise NotFound(description="List not found")
 
-    return render_template('list.html', lst=lst)    
+    return render_template('list.html', lst=lst)
+
+@app.route("/lists/<list_id>", methods=["POST"])
+def create_todo(list_id):
+    todo_title = request.form["todo"].strip()
+    lst = find_list_by_id(list_id, session['lists'])
+    if not lst:
+        raise NotFound(description="List not found")
+
+    error = error_for_todo(todo_title)
+    if error:
+        flash(error, "error")
+        return render_template('list.html', lst=lst)
+    
+    lst['todos'].append({
+        'id': str(uuid4()),
+        'title': todo_title,
+        'completed': False,
+    })
+
+    flash("The todo has been created.", "success")
+    session.modified = True
+    return redirect(url_for('show_list', list_id=list_id))
+
+@app.route("/lists/<list_id>/todos/<todo_id>/toggle", methods=["POST"])
+def update_todo_status(list_id, todo_id):
+    lst = find_list_by_id(list_id, session['lists'])
+    if not lst:
+        raise NotFound(description="List not found")
+
+    todo = find_todo_by_id(todo_id, lst['todos'])
+    if not todo:
+        raise NotFound(description="Todo not found")
+    
+    todo['completed'] = (request.form['completed'] == 'True') # automatically converts to boolean value after evaluation
+    flash(f"Todo '{todo['title']}' has been updated.", "success")
+    session.modified = True
+    return redirect(url_for('show_list', list_id=list_id)) # don't need todo_id because handled w/ temp var in Jinja2 template
+
+@app.route("/lists/<list_id>/todos/<todo_id>/delete", methods=["POST"])
+def delete_todo(list_id, todo_id):
+    lst = find_list_by_id(list_id, session['lists'])
+    if not lst:
+        raise NotFound(description="List not found")
+
+    todo = find_todo_by_id(todo_id, lst['todos'])
+    if not todo:
+        raise NotFound(description="Todo not found")
+    
+    '''
+    find todo by id, then determine its index position in lst['todos']
+    remove the todo at that index position, printing title in success message
+    '''
+    todo_index = lst['todos'].index(todo)
+    deleted_todo = lst['todos'].pop(todo_index)
+    flash(f"Todo '{deleted_todo['title']}' has been DELETED.", "success")
+    session.modified = True
+    return redirect(url_for('show_list', list_id=list_id))
+
+@app.route("/lists/<list_id>/complete_all", methods=["POST"])
+def complete_all_todos(list_id):
+    lst = find_list_by_id(list_id, session['lists'])
+    if not lst:
+        raise NotFound(description="List not found")
+    
+    mark_all_completed(lst)
+    
+    flash(f"All Todos have been marked COMPLETED.", "success")
+    session.modified = True
+    return redirect(url_for('show_list', list_id=list_id))
 
 if __name__ == "__main__":
     app.run(debug=True, port=5003)
--- todos/utils.py
+++ todos/utils.py
@@ -6,5 +6,18 @@ def error_for_list_title(title, lists):
     else:
         return None
 
+def error_for_todo(title):
+    if not 1 <= len(title) <= 100:
+        return "Todo title must be between 1 and 100 characters."
+
 def find_list_by_id(list_id, lists):
     return next((lst for lst in lists if lst['id'] == list_id), None)
+
+def find_todo_by_id(todo_id, todos):
+    return next((todo for todo in todos if todo['id'] == todo_id), None)
+
+def mark_all_completed(lst):
+    for todo in lst['todos']:
+        todo['completed'] = True
+    
+    return None
